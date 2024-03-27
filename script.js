7c9f02dd13289bd0885030b9551996a5e3e06c95
MatiZeglen <matizeglen2003@gmail.com>
2024-03-27 12:15:25 +0100

    WiP

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const display = document
+const display = document.getElementsByClassName("display")
+const calculate = document.getElementsByClassName("calculate")
+const clearDisplay = document.getElementsByClassName("clearDisplay")
+function appendToDisplay(input)
+{
+
+}
+function clearDisplay()
+{
+
+}
+function calculate()
+{
+    
+}
